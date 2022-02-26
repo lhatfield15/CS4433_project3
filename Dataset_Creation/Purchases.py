240e1dd0d272e5ac4baf76556baee4d5d2a6b91c
gmala4637 <gmala4637@gmail.com>
2022-02-26 16:00:10 -0500

    problem 2 data creation

---
+++ Dataset_Creation/Purchases.py
@@ -0,0 +1,25 @@
+import csv
+import names
+import random
+import string
+
+ROWS = 5000000
+descriptions = ["Home Furningish",
+                "Business Expenses and Supplies",
+                "Child School Supplies",
+                "Groceries and Toiletries",
+                "Clothes and Shoes",
+                ]
+
+with open('Purchases.csv', 'w', newline='') as csvfile:
+    csvfile.truncate(0)
+    writer = csv.writer(csvfile, delimiter=',')
+    # writer.writerow(['TransID', 'CustID', 'TransTotal', 'TransNumItems', 'TransDesc'])
+    for i in range(ROWS):
+        writer.writerow([
+            i + 1,
+            random.randint(1, 50000), 
+            random.uniform(10, 2000),
+            random.randint(1, 15),
+            descriptions[random.randint(0, 4)]
+        ])
