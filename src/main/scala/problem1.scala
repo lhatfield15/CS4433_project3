249b040ba8925f28e2597eb1b2f86ee1731de6b1
mchalmers <marcus18ca@gmail.com>
2022-02-28 23:17:24 -0500

    p1 q1 works

--- src/main/scala/problem1.scala
+++ src/main/scala/problem1.scala
@@ -1,7 +1,4 @@
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.SparkContext
-import org.apache.spark.rdd.RDD
-
 import math.{pow, sqrt}
 import scala.collection.mutable.ArrayBuffer
 
@@ -11,22 +8,24 @@ object problem1 extends Serializable {
     sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2))
   }
 
-  def filter_infected(person: String, infectedLookup: Array[String]): Boolean = {
+  def count_people_within_6ft(person: String, infectedLookup: Array[String]): Int = {
     //get person cords
     val personCords = person.split(",")
     val pX = personCords(1).toFloat
     val pY = personCords(2).toFloat
-    //see if our person is within 6 ft of an infected person
+    var count = 0
+    //see if our person is within 6 ft of another person
     infectedLookup.foreach(infected_person => {
+      //get infected coords
       val infPersonCords = infected_person.split(",")
       val x = infPersonCords(1).toFloat
       val y = infPersonCords(2).toFloat
       //if our person was close to an infected, keep him
       if (get_distance(x, y, pX, pY) < 6) {
-        return true
+        count += 1
       }
     })
-    false
+    count
   }
 
   def cartesian_to_square_grid_coords(x: Float, y: Float): (Float, Float) ={
@@ -45,6 +44,7 @@ object problem1 extends Serializable {
   }
 
   def mapped_square_infected(coord: String): TraversableOnce[(String, String)] = {
+    //get coord of infected person
     val coordArr = coord.split(",")
     val x = coordArr(1).toFloat
     val y = coordArr(2).toFloat
@@ -68,36 +68,39 @@ object problem1 extends Serializable {
       }
     }
 
+    //map the square coord to the info of the infected person with an infected tag
     val grid_report = grid_cells.map(square => {
       (square, coord + ",infected")
     })
     grid_report
   }
 
-  def reduce_grid_cells(values: Iterable[String]): scala.collection.mutable.Set[String] = {
+  def reduce_grid_cells(values: Iterable[String]): ArrayBuffer[(String, Int)] = {
     //sort the infected and people
     val infected = ArrayBuffer[String]()
     val people = ArrayBuffer[String]()
     values.foreach(value => {
+      //get the info of the individual
       val split_values = value.split(",")
       val id = split_values(0)
       val x = split_values(1)
       val y = split_values(2)
       val infection_state = split_values(3)
+      //sort individual into infect or person
       if(infection_state == "infected"){
         infected += id + "," + x + "," + y
       }
       else{
-        val people_string = id + "," + x + "," + y
-        people += people_string
+        people += id + "," + x + "," + y
       }
     })
 
-    var exposed_people = scala.collection.mutable.Set[String]()
-    people.foreach(person => {
-      if(filter_infected(person, infected.toArray)) {
-        exposed_people += person
-      }
+    //get the count of the near people for each infected person
+    var exposed_people = ArrayBuffer[(String, Int)]()
+    infected.foreach(infected => {
+      val num_contacts = count_people_within_6ft(infected, people.toArray)
+      val value = (infected, num_contacts) // to make scala happy
+      exposed_people += value
     })
     exposed_people
   }
@@ -110,7 +113,10 @@ object problem1 extends Serializable {
     //load the infected file to each worker
     val infectedLookup = sc.broadcast(InfectedSmall.collect)
     // filter out any people who were not within 6ft of an infected person
-    val result = sc.parallelize[String](People.collect()).filter( person => { filter_infected(person, infectedLookup.value)}).distinct.collect
+    val result = sc.parallelize[String](People.collect()).filter( person => {
+      //only care if theres at least one infected person near (x>0)
+      count_people_within_6ft(person, infectedLookup.value) > 0
+    }).distinct.collect
     result.foreach(println)
   }
 
@@ -127,16 +133,22 @@ object problem1 extends Serializable {
     //read files
     val People = sc.textFile("Dataset_Creation/PEOPLE.csv")
     val InfectedLarge = sc.textFile("Dataset_Creation/INFECTED-LARGE.csv")
+
+    //Take PEOPLE and return (key , value) (coordinate , id,x,y,healthy)
     val mapped_people = sc.parallelize[String](People.collect())
       .map(x => {map_square_people(x)}).distinct()
 
+    //Take InfectedLarge and return multiple (key , value)'s (if in 6ft) (coordinate , id,x,y,infected)
     val mapped_infected = sc.parallelize[String](InfectedLarge.collect())
       .flatMap(x => {mapped_square_infected(x)}).distinct()
 
+    //Combine all values by key (Key, Arr[Values])
+    //Map -> take value array and return number of infected near if any (id,x,y , number)
     val combine_all = mapped_people.union(mapped_infected)
       .groupByKey()
       .flatMap(x => reduce_grid_cells(x._2)).distinct()
 
+    //print
     combine_all.collect().foreach(println)
   }
 
